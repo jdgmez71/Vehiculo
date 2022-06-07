9c9a5a29df0a07c888a4bdd7296ffd73bd26880e
jdgmez71 <101287277+jdgmez71@users.noreply.github.com>
2022-06-07 17:43:31 -0500

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Datos myData = new Datos();
+            Console.WriteLine("Insert the fruit name:");
+            myData.setName(Console.ReadLine());
+            Console.WriteLine("Insert the fruit price:");
+            myData.setPrice(double.Parse(Console.ReadLine()));
+            Console.WriteLine(myData.ToString());
+        }
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Datos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Datos
+    {
+        public string Name;
+        public double Price;
+
+        public Datos (string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public Datos()
+        {
+        }
+
+        public void setName(string Name) { this.Name = Name; }
+        public string getName()
+        { return Name; }
+        
+        public void setPrice(double Price)
+        { this.Price = Price; }
+
+        public double getPrice()
+        {
+            return Price;
+        }
+        public override string ToString()
+        {
+            return $"Fruit: {Name} Price: {Price}";
+        }
+
+    }
+}
